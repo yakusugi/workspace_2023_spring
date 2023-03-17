1b85aaf36426bc39945eac3dbc2639c060cc6749
Yakusugi <yosukeito1987@gmail.com>
2023-03-17 11:09:07 +0900

    second commit

--- src/main/java/com/example/budget_tracker_spring/BudgetTrackerSpringApplication.java
+++ src/main/java/com/example/budget_tracker_spring/BudgetTrackerSpringApplication.java
@@ -2,8 +2,12 @@ package com.example.budget_tracker_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.budget_tracker_spring.repository")
+@EntityScan(basePackages = "com.example.budget_tracker_spring.entity")
 public class BudgetTrackerSpringApplication {
 
 	public static void main(String[] args) {
