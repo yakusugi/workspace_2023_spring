87f70d7e08ca56a25e22283016ca5e540c0e4b42
Yakusugi <yosukeito1987@gmail.com>
2023-03-17 19:53:01 +0900

    fifth commit

--- src/main/java/com/example/budget_tracker_spring/entity/User.java
+++ src/main/java/com/example/budget_tracker_spring/entity/User.java
@@ -2,12 +2,13 @@ package com.example.budget_tracker_spring.entity;
 
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+import jakarta.persistence.*;
 
 import lombok.Data;
 /**
